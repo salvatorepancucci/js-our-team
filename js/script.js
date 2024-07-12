b6a866769bfe2ae5afa22592e459edd050403791
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-12 13:09:51 +0200

    Mileston 0 done

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+const teamMembers = [
+    {
+        nome: 'Mario Rossi',
+        ruolo: 'Developer',
+        foto: 'mario.jpg'
+    },
+    {
+        nome: 'Luigi Bianchi',
+        ruolo: 'Designer',
+        foto: 'luigi.jpg'
+    },
+    {
+        nome: 'Anna Verdi',
+        ruolo: 'Project Manager',
+        foto: 'anna.jpg'
+    },
+    {
+        nome: 'Laura Neri',
+        ruolo: 'Tester',
+        foto: 'laura.jpg'
+    }
+];
